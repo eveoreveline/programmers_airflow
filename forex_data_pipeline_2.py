7974cf9e1f41b59185846d25891bfa66f0a7b7ee
Eve <99390776+eveoreveline@users.noreply.github.com>
2022-11-30 22:43:56 +0900

    [Udemy] airflow 실습 dag parameter 파일
    유데미의 'Apache Airflow: The Hands-On Guide' 참고.

---
+++ forex_data_pipeline_2.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+
+from datetime import datetime, timedelta
+
+default_args = {
+        "owner": "airflow",
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "email": "admin@localhost.com",
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5)
+}
+
+with DAG("forex_data_pipeline_2", start_date=datetime(2021, 1, 1), default_args=default_args, catchup=False) as dag:
